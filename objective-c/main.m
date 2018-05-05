751a16fd7726ddfbde0dabd2ffb41bb78f1a1b52
Song Zhou <zhousong1993@gmail.com>
2018-05-05 23:35:30 +0800

    Initial Commit

---
+++ objective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objective-c
+//
+//  Created by songzhou on 2018/5/5.
+//  Copyright © 2018/5/5 songzhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
